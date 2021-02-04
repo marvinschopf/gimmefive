f8120515c985b888b3ecfb56baa865893e981a16
Marvin Schopf <marvin@schopf.biz>
2021-02-04 23:20:32 +0100

    Prettify code

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ import webpack from "webpack";
 import path from "path";
 
 module.exports = {
-    entry: './index.js',
-    context: path.resolve(__dirname + "src"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "highfive.min.js",
-        library: 'Highfive',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                include: resolve(__dirname, 'src'),
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            babelrc: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    "resolve": {
-        extensions: ['.js', '.jsx'],
-        "alias": {
-          "react": "preact-compat",
-          "react-dom": "preact-compat"
-        }
-      }
-}
+	entry: "./index.js",
+	context: path.resolve(__dirname + "src"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "highfive.min.js",
+		library: "Highfive",
+		libraryTarget: "umd",
+		umdNamedDefine: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				include: resolve(__dirname, "src"),
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							babelrc: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+		alias: {
+			react: "preact-compat",
+			"react-dom": "preact-compat",
+		},
+	},
+};
